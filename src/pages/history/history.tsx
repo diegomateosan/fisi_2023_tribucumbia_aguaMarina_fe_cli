2f4d202a6623a28710315b69343ec3a5a4532744
asem210 <gustavoadrianpazos@gmail.com>
2022-07-28 20:09:39 -0500

    historial avance

--- src/pages/history/history.tsx
+++ src/pages/history/history.tsx
@@ -4,20 +4,20 @@ import { NavBarDefault } from "../../components/navBar/navBar";
 
 import { HistoryHeader } from "../../components/header/header";
 
+import { FilaDefault } from "../../components/tabla/tabla";
+
+import { Footer } from "../../components/footer/footer";
+
+import { Searchbox } from "../../components/searchBox/searchBox";
 
 import "./history.css";
 export const History: React.FC<{
-
   userState: boolean;
   setUserState: (txt: boolean) => void;
   setUserValue: (txt: string) => void; // corro
   userValue: string; // correo
- 
-
-}> = ({ userState, setUserState, setUserValue, userValue}) => {
+}> = ({ userState, setUserState, setUserValue, userValue }) => {
   return (
-    
-
     <div className="app-container-history">
       <NavBarDefault
         userState={userState}
@@ -26,7 +26,18 @@ export const History: React.FC<{
         userValue={userValue}
       />
       <HistoryHeader />
-     
+      <div className="app-container-searchbox">
+        <Searchbox
+          placeholder="Filtrar por nombre, categoria, fecha, etc."
+          handleSearch={() => alert("Buscado")}
+        />
+      </div>
+
+      <div className="app-container-table">
+        <FilaDefault />
+      </div>
+
+      <Footer />
     </div>
   );
 };
